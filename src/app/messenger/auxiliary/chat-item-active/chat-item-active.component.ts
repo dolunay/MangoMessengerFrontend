ef3bac96e63875efa0bcd1f3473de22904196f90
Petro Kolosov <pkolosov@openskydata.com>
2021-07-20 19:55:11 +0200

    chat parameters added

--- src/app/messenger/auxiliary/chat-item-active/chat-item-active.component.ts
+++ src/app/messenger/auxiliary/chat-item-active/chat-item-active.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat-item-active',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatItemActiveComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  // @ts-ignore
+  @Input() chatName: string;
+  // @ts-ignore
+  @Input() lastMessage: string;
+  // @ts-ignore
+  @Input() lastMessageDateTime: string;
 
 }
