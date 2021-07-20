32f30a6fdfa43f450e53f4a59dd3fd9e333be65e
Petro Kolosov <pkolosov@openskydata.com>
2021-07-20 20:00:50 +0200

    group component params added

--- src/app/messenger/auxiliary/group-item/group-item.component.ts
+++ src/app/messenger/auxiliary/group-item/group-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-group-item',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupItemComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  // @ts-ignore
+  @Input() groupName: string;
+  // @ts-ignore
+  @Input() lastMessage: string;
+  // @ts-ignore
+  @Input() lastMessageAuthor: string;
+  // @ts-ignore
+  @Input() lastMessageDateTime: string;
 }
