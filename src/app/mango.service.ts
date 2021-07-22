ae1aafb2050f1f4f58b878bcbc138b33884a3cc1
Petro Kolosov <pkolosov@openskydata.com>
2021-07-22 19:31:19 +0200

    updated

--- src/app/mango.service.ts
+++ src/app/mango.service.ts
@@ -8,7 +8,7 @@ import {ILoginResponse} from "../types/Auth/Responses/ILoginResponse";
 import {IRefreshTokenResponse} from "../types/Auth/Responses/IRefreshTokenResponse";
 import {Tokens} from "../consts/Tokens";
 import {IRegisterResponse} from "../types/Auth/Responses/IRegisterResponse";
-import {AuthRoutes, ChatsRoutes} from "../consts/Routes";
+import {ApiRoute, AuthRoutes, ChatsRoutes, MessagesRoutes} from "../consts/Routes";
 import {IVerifyPhoneCodeResponse} from "../types/Auth/Responses/IVerifyPhoneCodeResponse";
 import {IGetUserChatsResponse} from "../types/Chats/Responses/IGetUserChatsResponse";
 import {IGetChatMessagesResponse} from "../types/Messages/Responses/IGetChatMessagesResponse";
@@ -20,26 +20,24 @@ import {ISendMessageResponse} from "../types/Messages/Responses/ISendMessageResp
   providedIn: 'root'
 })
 export class MangoService {
-  private mangoApiUrl = 'https://mango-messenger-app.herokuapp.com/';
-
   constructor(private httpClient: HttpClient) {
   }
 
   register(command: RegisterCommand): Observable<IRegisterResponse> {
-    return this.httpClient.post<IRegisterResponse>(this.mangoApiUrl + AuthRoutes.postRegister, command);
+    return this.httpClient.post<IRegisterResponse>(ApiRoute.apiDomain + AuthRoutes.postRegister, command);
   }
 
   verifyPhone(command: VerifyPhoneCommand): Observable<IVerifyPhoneCodeResponse> {
-    return this.httpClient.post<IVerifyPhoneCodeResponse>(this.mangoApiUrl + AuthRoutes.getVerifyPhone, command);
+    return this.httpClient.post<IVerifyPhoneCodeResponse>(ApiRoute.apiDomain + AuthRoutes.getVerifyPhone, command);
   }
 
   login(command: LoginCommand): Observable<ILoginResponse> {
-    return this.httpClient.post<ILoginResponse>(this.mangoApiUrl + AuthRoutes.postLogin, command,
+    return this.httpClient.post<ILoginResponse>(ApiRoute.apiDomain + AuthRoutes.postLogin, command,
       {withCredentials: true});
   }
 
   refreshToken(request: RefreshTokenCommand): Observable<IRefreshTokenResponse> {
-    return this.httpClient.post<IRefreshTokenResponse>(this.mangoApiUrl + 'api/auth/refresh-token', request);
+    return this.httpClient.post<IRefreshTokenResponse>(ApiRoute.apiDomain + AuthRoutes.postRefreshToken, request);
   }
 
   getUserChats(): Observable<IGetUserChatsResponse> {
@@ -47,7 +45,7 @@ export class MangoService {
       headers: new HttpHeaders()
         .set('Authorization', `Bearer ${localStorage.getItem(Tokens.accessToken)}`)
     };
-    return this.httpClient.get<IGetUserChatsResponse>(this.mangoApiUrl + ChatsRoutes.getChats, header);
+    return this.httpClient.get<IGetUserChatsResponse>(ApiRoute.apiDomain + ChatsRoutes.getChats, header);
   }
 
   getChatMessages(chatId: number): Observable<IGetChatMessagesResponse> {
@@ -55,7 +53,7 @@ export class MangoService {
       headers: new HttpHeaders()
         .set('Authorization', `Bearer ${localStorage.getItem(Tokens.accessToken)}`)
     };
-    return this.httpClient.get<IGetChatMessagesResponse>(this.mangoApiUrl + 'api/messages/' + chatId, header);
+    return this.httpClient.get<IGetChatMessagesResponse>(ApiRoute.apiDomain + MessagesRoutes.getChatMessages + chatId, header);
   }
 
   sendMessage(request: SendMessageCommand): Observable<ISendMessageResponse> {
@@ -63,6 +61,6 @@ export class MangoService {
       headers: new HttpHeaders()
         .set('Authorization', `Bearer ${localStorage.getItem(Tokens.accessToken)}`)
     };
-    return this.httpClient.post<ISendMessageResponse>(this.mangoApiUrl + 'api/messages/', request, header);
+    return this.httpClient.post<ISendMessageResponse>(ApiRoute.apiDomain + MessagesRoutes.postMessage, request, header);
   }
 }
