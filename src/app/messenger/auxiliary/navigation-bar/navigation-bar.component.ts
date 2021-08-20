38bde30103c460a444e562cd57cc590c23f4874d
Petro Kolosov <pkolosov@openskydata.com>
2021-08-20 15:57:22 +0200

    updated

--- src/app/messenger/auxiliary/navigation-bar/navigation-bar.component.ts
+++ src/app/messenger/auxiliary/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  navigateToChats() : void {
+    this.router.navigateByUrl('main').then(r => r);
+  }
+
+  navigateToContacts(): void {
+    this.router.navigateByUrl('contacts').then(r => r);
+  }
+
+  navigateToSettings() : void {
+    this.router.navigateByUrl('profile-settings').then(r => r);
+  }
 }
