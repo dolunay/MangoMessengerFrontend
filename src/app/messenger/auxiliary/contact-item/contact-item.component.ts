6beb922ba0e272d077d1a441b104082267364d09
Petro Kolosov <pkolosov@openskydata.com>
2021-08-20 15:36:37 +0200

    contacts component updated

--- src/app/messenger/auxiliary/contact-item/contact-item.component.ts
+++ src/app/messenger/auxiliary/contact-item/contact-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact-item',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactItemComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  @Input() contactName = '';
+  @Input() contactAddress = '';
 
   ngOnInit(): void {
   }
