679c27bd4087477587d8e9040e660ad37c97b040
Petro Kolosov <pkolosov@openskydata.com>
2021-08-22 22:16:16 +0200

    add contacts implelented

--- src/types/interfaces/IContactsService.ts
+++ src/types/interfaces/IContactsService.ts
@@ -1,6 +1,9 @@
 import {Observable} from "rxjs";
 import {IGetContactsResponse} from "../responses/IGetContactsResponse";
+import {IAddContactResponse} from "../responses/IAddContactResponse";
 
 export interface IContactsService {
   getContacts(): Observable<IGetContactsResponse>;
+
+  postAddContact(userId: string): Observable<IAddContactResponse>;
 }
