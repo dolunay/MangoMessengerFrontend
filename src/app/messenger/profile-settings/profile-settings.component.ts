cc1a71f7e168d46c852c5f8804a57d7ec41cee88
Petro Kolosov <pkolosov@openskydata.com>
2021-08-23 17:54:00 +0200

    profile settings refactors

--- src/app/messenger/profile-settings/profile-settings.component.ts
+++ src/app/messenger/profile-settings/profile-settings.component.ts
@@ -3,6 +3,7 @@ import {UsersService} from "../../services/users.service";
 import {IGetUserResponse} from "../../../types/responses/IGetUserResponse";
 import {UpdateUserInformationCommand} from "../../../types/requests/UpdateUserInformationCommand";
 import {ChangePasswordCommand} from "../../../types/requests/ChangePasswordCommand";
+import {IUser} from "../../../types/models/IUser";
 
 @Component({
   selector: 'app-profile-settings',
@@ -14,20 +15,24 @@ export class ProfileSettingsComponent implements OnInit {
   constructor(private userService: UsersService) {
   }
 
-  firstName: string = '';
-  lastName: string = '';
-  displayName: string = '';
-  phoneNumber: string = '';
-  birthdayDate: string = '';
-  email: string = '';
-  website: string = '';
-  username: string = '';
-  bio: string = '';
-  address: string = '';
-  facebook: string = '';
-  twitter: string = '';
-  instagram: string = '';
-  linkedIn: string = '';
+  currentUser: IUser = {
+    address: "",
+    bio: "",
+    birthdayDate: "",
+    displayName: "",
+    email: "",
+    facebook: "",
+    firstName: "",
+    instagram: "",
+    lastName: "",
+    linkedIn: "",
+    phoneNumber: "",
+    twitter: "",
+    userId: "",
+    username: "",
+    website: ""
+  };
+
   currentPassword = '';
   newPassword = '';
   repeatNewPassword = '';
@@ -38,59 +43,47 @@ export class ProfileSettingsComponent implements OnInit {
 
   initializeView(): void {
     this.userService.getCurrentUser().subscribe((data: IGetUserResponse) => {
-      this.firstName = data.user.firstName;
-      this.lastName = data.user.lastName;
-      this.displayName = data.user.displayName;
-      this.phoneNumber = data.user.phoneNumber;
-      this.birthdayDate = data.user.birthdayDate;
-      this.email = data.user.email;
-      this.website = data.user.website;
-      this.username = data.user.username;
-      this.bio = data.user.bio;
-      this.address = data.user.address;
-      this.facebook = data.user.facebook;
-      this.twitter = data.user.twitter;
-      this.instagram = data.user.instagram;
-      this.linkedIn = data.user.linkedIn;
+      this.currentUser = data.user;
       this.currentPassword = '';
       this.newPassword = '';
       this.repeatNewPassword = '';
     }, error => {
-      alert(error.message);
+      alert(error.error.ErrorMessage);
     })
   }
 
   saveAccountInfo(): void {
-    const command = new UpdateUserInformationCommand(this.firstName,
-      this.lastName,
-      this.displayName,
-      this.phoneNumber,
-      this.birthdayDate,
-      this.email,
-      this.username,
-      this.bio,
-      this.address);
+    const command = new UpdateUserInformationCommand(this.currentUser.firstName,
+      this.currentUser.lastName,
+      this.currentUser.displayName,
+      this.currentUser.phoneNumber,
+      this.currentUser.birthdayDate,
+      this.currentUser.email,
+      this.currentUser.username,
+      this.currentUser.bio,
+      this.currentUser.address);
 
-    command.website = this.website;
+    command.website = this.currentUser.website;
 
-    this.userService.putUpdateUserInformation(command).subscribe((_) => {
-
+    this.userService.putUpdateUserInformation(command).subscribe((data) => {
+      this.initializeView();
+      alert(data.message);
     }, error => {
-      alert(error.message);
+      alert(error.error.ErrorMessage);
     })
   };
 
   saveSocialMediaInfo(): void {
     const command = UpdateUserInformationCommand.CreateEmptyCommand();
-    command.facebook = this.facebook;
-    command.twitter = this.twitter;
-    command.instagram = this.instagram;
-    command.linkedIn = this.linkedIn;
+    command.facebook = this.currentUser.facebook;
+    command.twitter = this.currentUser.twitter;
+    command.instagram = this.currentUser.instagram;
+    command.linkedIn = this.currentUser.linkedIn;
 
     this.userService.putUpdateUserInformation(command).subscribe((_) => {
 
     }, error => {
-      alert(error.message);
+      alert(error.error.ErrorMessage);
     })
   }
 
