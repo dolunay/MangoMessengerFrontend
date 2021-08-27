b135bb2d387e19a9378f8bef67e31ed6d0f9eeef
Petro Kolosov <pkolosov@openskydata.com>
2021-08-27 21:52:44 +0200

    mango 100 edits

---
+++ src/app/messenger/auxiliary/verify-email-note/verify-email-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-email-note',
+  templateUrl: './verify-email-note.component.html',
+  styleUrls: ['./verify-email-note.component.scss']
+})
+export class VerifyEmailNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
