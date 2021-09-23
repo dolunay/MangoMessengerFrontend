0a5775d88b243cc43092847a31c41b1a5082e99b
Petro Kolosov <pkolosov@openskydata.com>
2021-09-23 18:46:08 +0200

    updated

--- src/app/messenger/auxiliary/chat-footer/chat-footer.component.ts
+++ src/app/messenger/auxiliary/chat-footer/chat-footer.component.ts
@@ -3,37 +3,71 @@ import {MessagesService} from "../../../services/messages.service";
 import {SendMessageCommand} from "../../../../types/requests/SendMessageCommand";
 import {IChat} from "../../../../types/models/IChat";
 import {CommunityType} from "../../../../types/enums/CommunityType";
+import {DocumentsService} from "../../../services/documents.service";
 
 @Component({
-    selector: 'app-chat-footer',
-    templateUrl: './chat-footer.component.html'
+  selector: 'app-chat-footer',
+  templateUrl: './chat-footer.component.html'
 })
 export class ChatFooterComponent {
 
-    constructor(private messageService: MessagesService) {
+  constructor(private messageService: MessagesService,
+              private documentService: DocumentsService) {
+  }
+
+  currentMessageText: string = '';
+  attachmentName: string | null = '';
+
+  attachment!: File | null;
+
+  @Input() chat: IChat = {
+    chatId: "",
+    chatLogoImageUrl: "",
+    communityType: CommunityType.DirectChat,
+    description: "",
+    isArchived: false,
+    isMember: false,
+    lastMessage: null,
+    membersCount: 0,
+    title: ""
+  }
+
+  onMessageSendClick(): void {
+    let attachmentUrl: string | null = null;
+
+    if (this.attachment) {
+      const formData = new FormData();
+      formData.append("formFile", this.attachment);
+      this.documentService.uploadDocument(formData).subscribe(response => {
+        attachmentUrl = response.fileUrl;
+      }, error => {
+        alert(error.error.ErrorMessage);
+      })
     }
 
-    currentMessageText: string = '';
+    const sendMessageCommand = new SendMessageCommand(this.currentMessageText, this.chat.chatId);
 
-    @Input() chat: IChat = {
-        chatId: "",
-        chatLogoImageUrl: "",
-        communityType: CommunityType.DirectChat,
-        description: "",
-        isArchived: false,
-        isMember: false,
-        lastMessage: null,
-        membersCount: 0,
-        title: ""
+    if (attachmentUrl) {
+      sendMessageCommand.setAttachmentUrl(attachmentUrl);
     }
 
-    onMessageSendClick(): void {
-        const sendMessageCommand = new SendMessageCommand(this.currentMessageText, this.chat.chatId);
-        this.messageService.sendMessage(sendMessageCommand).subscribe(_ => {
-            this.currentMessageText = '';
-        }, error => {
-            alert(error.error.ErrorMessage);
-        })
+    this.messageService.sendMessage(sendMessageCommand).subscribe(_ => {
+      this.currentMessageText = '';
+      this.attachmentName = null;
+      this.attachment = null;
+    }, error => {
+      alert(error.error.ErrorMessage);
+    })
 
+  }
+
+  attachDocument(event: any): void {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.attachment = file;
+      this.attachmentName = file.name;
+      console.log(this.attachment);
     }
+  }
 }
