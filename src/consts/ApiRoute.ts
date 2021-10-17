af0d36c6578263c4f0a2a639bee421db0a6d9d53
Petro Kolosov <pkolosov@openskydata.com>
2021-10-17 10:18:18 +0200

    route updated

--- src/consts/ApiRoute.ts
+++ src/consts/ApiRoute.ts
@@ -1,4 +1,4 @@
 ﻿export class ApiRoute {
-  //static route = 'https://mango-messenger-app.herokuapp.com/';
-  static route = 'https://localhost:5001/';
+  static route = 'https://mango-messenger-app.herokuapp.com/';
+  //static route = 'https://localhost:5001/';
 }
