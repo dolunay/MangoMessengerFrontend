3d23a1b2e3b5abc572fc2d1e757981159678183a
Petro Kolosov <pkolosov@openskydata.com>
2021-10-17 20:45:20 +0200

    auto unsubscribe in register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,14 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {SessionService} from "../../services/session.service";
 import {UsersService} from "../../services/users.service";
 import {RegisterCommand} from "../../../types/requests/RegisterCommand";
+import {Subscription} from "rxjs";
+import {AutoUnsubscribe} from "ngx-auto-unsubscribe";
 
+@AutoUnsubscribe()
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
 
   public registerCommand: RegisterCommand = {
     displayName: "Bo4a Account",
@@ -19,6 +22,8 @@ export class RegisterComponent {
     verificationMethod: 2
   };
 
+  protected registerSub$!: Subscription;
+
   constructor(private usersService: UsersService,
               private sessionService: SessionService,
               private route: ActivatedRoute,
@@ -26,14 +31,14 @@ export class RegisterComponent {
   }
 
   register(): void {
-
-    this.usersService.createUser(this.registerCommand).subscribe(registerResponse => {
+    this.registerSub$ = this.usersService.createUser(this.registerCommand).subscribe(registerResponse => {
       this.sessionService.writeAccessToken(registerResponse.accessToken);
       this.sessionService.writeRefreshToken(registerResponse.refreshToken);
       this.sessionService.writeUserId(registerResponse.userId);
       this.router.navigateByUrl('verify-email-note').then(r => r);
-    }, error => {
-      alert(error.error.ErrorMessage);
-    });
+    }, error => alert(error.error.ErrorMessage));
+  }
+
+  ngOnDestroy(): void {
   }
 }
