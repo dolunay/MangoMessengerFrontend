3d7e47303fc43c745cead755637d3ccb2946acf4
Petro Kolosov <pkolosov@openskydata.com>
2021-10-17 20:38:35 +0200

    registration logics updated

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {SessionService} from "../../services/session.service";
 import {UsersService} from "../../services/users.service";
-import {VerificationMethod} from "../../../types/enums/VerificationMethod";
 import {RegisterCommand} from "../../../types/requests/RegisterCommand";
 
 @Component({
@@ -11,34 +10,27 @@ import {RegisterCommand} from "../../../types/requests/RegisterCommand";
 })
 export class RegisterComponent {
 
-  phoneNumber = '';
-  email = '';
-  password = '';
-  displayName = '';
-  verificationMethod = VerificationMethod.Email;
-  termsAccepted = false;
+  public registerCommand: RegisterCommand = {
+    displayName: "Bo4a Account",
+    email: "kolosovp94@gmail.com",
+    password: "z[?6dMR#xmp=nr6q",
+    phoneNumber: "380974913851980",
+    termsAccepted: false,
+    verificationMethod: 2
+  };
 
-  verificationMethods = [VerificationMethod.Phone, VerificationMethod.Email];
-
-  constructor(private usersService: UsersService, private sessionService: SessionService,
-              private route: ActivatedRoute, private router: Router) {
+  constructor(private usersService: UsersService,
+              private sessionService: SessionService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   register(): void {
-    const verificationMethod = this.verificationMethod === VerificationMethod.Email ? 2 : 1;
 
-    const command = new RegisterCommand(this.phoneNumber, this.email, this.displayName,
-      this.password, verificationMethod, this.termsAccepted);
-
-    this.usersService.createUser(command).subscribe((data) => {
-      this.sessionService.writeAccessToken(data.accessToken);
-      this.sessionService.writeRefreshToken(data.refreshToken);
-      this.sessionService.writeUserId(data.userId);
-      if (this.verificationMethod === VerificationMethod.Phone) {
-        this.router.navigateByUrl('verify-phone').then(r => r);
-        return;
-      }
-
+    this.usersService.createUser(this.registerCommand).subscribe(registerResponse => {
+      this.sessionService.writeAccessToken(registerResponse.accessToken);
+      this.sessionService.writeRefreshToken(registerResponse.refreshToken);
+      this.sessionService.writeUserId(registerResponse.userId);
       this.router.navigateByUrl('verify-email-note').then(r => r);
     }, error => {
       alert(error.error.ErrorMessage);
