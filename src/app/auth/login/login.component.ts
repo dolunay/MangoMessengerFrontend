f1035005e18e4fdde49f655a8f573f2100d3c5d0
Petro Kolosov <pkolosov@openskydata.com>
2021-10-17 21:18:35 +0200

    login component updated

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,39 +3,38 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {SessionService} from "../../services/session.service";
 import {LoginCommand} from "../../../types/requests/LoginCommand";
 import {Subscription} from "rxjs";
+import {AutoUnsubscribe} from "ngx-auto-unsubscribe";
 
+@AutoUnsubscribe()
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnDestroy {
 
-  emailOrPhone = '';
-  password = '';
-
-  subscriptions: Subscription[] = [];
-
   constructor(private authService: SessionService,
               private route: ActivatedRoute,
               private router: Router) {
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(x => x.unsubscribe());
-  }
+  protected loginSub$!: Subscription;
+
+  public loginCommand: LoginCommand = {
+    emailOrPhone: "kolosovp94@gmail.com",
+    password: "z[?6dMR#xmp=nr6q"
+  };
 
   login(): void {
-    let loginSub = this.authService.createSession(new LoginCommand(this.emailOrPhone, this.password))
-      .subscribe(data => {
+    this.loginSub$ = this.authService.createSession(this.loginCommand).subscribe(data => {
+
         this.authService.writeAccessToken(data.accessToken);
         this.authService.writeRefreshToken(data.refreshToken);
         this.authService.writeUserId(data.userId);
-        console.log(data);
-        this.router.navigateByUrl('main').then(r => r);
-      }, error => {
-        alert(error.error.ErrorMessage)
-      });
 
-    this.subscriptions.push(loginSub);
+        this.router.navigateByUrl('main').then(r => r);
+      }, error => alert(error.error.ErrorMessage));
+  }
+
+  ngOnDestroy(): void {
   }
 }
