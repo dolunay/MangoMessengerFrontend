5734209556a455cc8ff78e479808e93b14b30662
Хачатур <xachulxx@gmail.com>
2021-12-27 21:20:48 +0300

    updated

--- src/app/messenger/main/main.component.ts
+++ src/app/messenger/main/main.component.ts
@@ -135,7 +135,7 @@ export class MainComponent implements OnInit, OnDestroy {
       }
 
       if (!this.activeChatId) {
-        this.getCurrentUserSub$ = this.userService.getCurrentUser().subscribe(data => this.currentUser = data.user);
+        this.getCurrentUserSub$ = this.userService.getUserById(localStorage.getItem("MangoUserID")).subscribe(data => this.currentUser = data.user);
       }
 
     }, error => {
